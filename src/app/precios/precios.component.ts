c2ef8b3c8fb54c8e4070fd7ce44012c260de1000
ianmajkut <ianedwinmajkut@gmail.com>
2021-10-07 11:26:18 -0300

    Finalización de agregar precio, subida a base de datos, obtención y muestra de la misma

--- src/app/precios/precios.component.ts
+++ src/app/precios/precios.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MensajesServiceService } from '../services/mensajes-service.service';
 
 @Component({
   selector: 'app-precios',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./precios.component.css']
 })
 export class PreciosComponent implements OnInit {
-
-  constructor() { }
+  formularioPrecio!: FormGroup
+  precios: any[] = new Array<any>()
+  constructor(private fb: FormBuilder, private db : AngularFirestore, private msg: MensajesServiceService) { }
 
   ngOnInit(): void {
+    this.formularioPrecio = this.fb.group({
+      nombre: ["", Validators.required],
+      costo: ["", Validators.required],
+      duracion: ["", Validators.required],
+      tipoDuracion: ["", Validators.required]
+    })
+    this.db.collection("precios").get().subscribe((resultado)=>{
+      resultado.docs.forEach((dato)=>{
+        let precio : any = dato.data()
+        precio.id = dato.id
+        precio.ref = dato.ref
+        this.precios.push(precio)
+      })
+    })
+  }
+
+  agregar(){
+    this.db.collection("precios").add(this.formularioPrecio.value).then(()=>{
+      this.msg.mensajeCorrecto("Se agregó correctamente el precio")
+      this.formularioPrecio.reset()
+    }).catch(()=>this.msg.mensajeError("Error","Error al agregar el precio"))
   }
 
 }
